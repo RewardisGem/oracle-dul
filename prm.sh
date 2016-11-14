66ef4a8fc58a3fdc963b92fedbb198254db0e7e2
Maclean Liu <liu.maclean@gmail.com>
2016-11-14 11:37:04 +0800

    Create prm.sh

---
+++ prm.sh
@@ -0,0 +1,3 @@
+echo "It's recommended that you can use JDK 1.6 or higher version"
+echo "Download latest JDK http://www.oracle.com/technetwork/java/javase/downloads/index.html"
+java -Xms1024M -jar prm.jar 1>>prm.log 2>&1
