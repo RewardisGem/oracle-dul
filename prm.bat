462cd5fc59f6db9be259a5410ef188a98b3078a9
Maclean Liu <liu.maclean@gmail.com>
2019-01-29 11:19:42 +0800

    Update prm.bat

--- prm.bat
+++ prm.bat
@@ -1,6 +1,13 @@
 @echo off
-echo ParnassusData推荐使用JDK 1.6或以上版本，若仅安装了gcj或jre可能无法正常使用PRM
+echo ParnassusData推荐使用JDK 1.8或以上版本，若仅安装了gcj或jre可能无法正常使用PRM
 echo 下载最新的JDK http://www.oracle.com/technetwork/java/javase/downloads/index.html
-echo It's recommended that you can use JDK 1.6 or higher version
+echo It's recommended that you can use JDK 1.8 or higher version
 echo Download latest JDK http://www.oracle.com/technetwork/java/javase/downloads/index.html
-java -Xms1024M -jar prm.jar 1>>prm.log 2>&1
+echo find user guide here:http://zcdn.parnassusdata.com/prm04.pdf
+echo 软件使用教程:http://zcdn.parnassusdata.com/prm03.pdf
+echo any problem please first check ./prm.log
+echo 任何问题请先检查软件目录下的prm.log
+
+
+java -Dfile.encoding=UTF-8 -jar prm.jar 1>>prm.log 2>&1
+pause
